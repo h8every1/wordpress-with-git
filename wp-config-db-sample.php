80bedc757fc5b1800968c036d33b8e956cfd61f0
h8every1 <h8every1@gmail.com>
2013-08-21 19:32:34 +0700

    all changes should be made into wp-config-db.php, and wp-config.php would in most times be intact

--- wp-config-db-sample.php
+++ wp-config-db-sample.php
@@ -16,8 +16,10 @@ if (WP_DEBUG) {
   @ini_set('error_reporting', E_ALL | E_STRICT);
   @ini_set('error_log', $_SERVER['DOCUMENT_ROOT'] . '/php_error.log');
   define('WP_DEBUG_LOG', true);
-  define('WP_DEBUG_DISPLAY', false);
+  define('WP_DEBUG_DISPLAY', true);
   define('SCRIPT_DEBUG', true);
+} else {
+  define('WP_DEBUG_DISPLAY', false);
 }
 
 /**
@@ -27,3 +29,28 @@ define( 'DB_NAME',     'database_name_here' );
 define( 'DB_USER',     'db_username_here'   );
 define( 'DB_PASSWORD', 'db_password_here'   );
 define( 'DB_HOST',     'localhost'          );
+
+/*
+ * Set theme
+ */
+define('WP_DEFAULT_THEME', 'set_your_theme_here');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/**#@-*/
--- wp-config-sample.php
+++ wp-config-sample.php
@@ -24,50 +24,12 @@ define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
 define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
 define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
 define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
-define('WP_DEFAULT_THEME', 'set_your_theme_here');
-
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-if (!defined('DB_NAME'))
-define('DB_NAME', 'database_name_here');
-
-/** MySQL database username */
-if (!defined('DB_USER'))
-define('DB_USER', 'username_here');
-
-/** MySQL database password */
-if (!defined('DB_PASSWORD'))
-define('DB_PASSWORD', 'password_here');
-
-/** MySQL hostname */
-if (!defined('DB_HOST'))
-define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
-/**#@-*/
 
 /**
  * WordPress Database Table prefix.
