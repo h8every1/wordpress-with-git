9a64a249b1e3436c60d4627c32c936d0f22bba96
Anton Syuvaev <h8every1@gmail.com>
2015-05-04 15:46:09 +0700

    code formatting

--- wp-config-local-sample.php
+++ wp-config-local-sample.php
@@ -10,30 +10,30 @@
  * Debug block, set WP_DEBUG to false to disable this on the live site
  */
 
-define('WP_DEBUG', true);
-if (WP_DEBUG) {
-  @ini_set('display_errors', true);
-  @ini_set('error_reporting', E_ALL | E_STRICT);
-  @ini_set('error_log', $_SERVER['DOCUMENT_ROOT'] . '/php_error.log');
-  define('WP_DEBUG_LOG', true);
-  define('WP_DEBUG_DISPLAY', true);
-  define('SCRIPT_DEBUG', true);
+define( 'WP_DEBUG', true );
+if ( WP_DEBUG ) {
+	@ini_set( 'display_errors', true );
+	@ini_set( 'error_reporting', E_ALL | E_STRICT );
+	@ini_set( 'error_log', $_SERVER['DOCUMENT_ROOT'] . '/php_error.log' );
+	define( 'WP_DEBUG_LOG', true );
+	define( 'WP_DEBUG_DISPLAY', true );
+	define( 'SCRIPT_DEBUG', true );
 } else {
-  define('WP_DEBUG_DISPLAY', false);
+	define( 'WP_DEBUG_DISPLAY', false );
 }
 
 /**
  * WordPress Database settings
  */
-define( 'DB_NAME',     'database_name_here' );
-define( 'DB_USER',     'db_username_here'   );
-define( 'DB_PASSWORD', 'db_password_here'   );
-define( 'DB_HOST',     'localhost'          );
+define( 'DB_NAME', 'database_name_here' );
+define( 'DB_USER', 'db_username_here' );
+define( 'DB_PASSWORD', 'db_password_here' );
+define( 'DB_HOST', 'localhost' );
 
-/*
- * Set theme
+/**
+ * Set default theme
  */
-define('WP_DEFAULT_THEME', 'theme');
+define( 'WP_DEFAULT_THEME', 'theme' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -44,21 +44,22 @@ define('WP_DEFAULT_THEME', 'theme');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define( 'AUTH_KEY', 'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY', 'put your unique phrase here' );
+define( 'LOGGED_IN_KEY', 'put your unique phrase here' );
+define( 'NONCE_KEY', 'put your unique phrase here' );
+define( 'AUTH_SALT', 'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT', 'put your unique phrase here' );
+define( 'NONCE_SALT', 'put your unique phrase here' );
 
 /**#@-*/
 
-/*
-* Define paths for WordPress in seaprate directory
-*/
-define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
-define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
-define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
-define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+/**
+ * Define paths for WordPress in seaprate directory
+ */
+
+define( 'WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress' );
+define( 'WP_HOME', 'http://' . $_SERVER['SERVER_NAME'] );
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content' );
