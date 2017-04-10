13c9a83c67b89bca296e28df39540121ec6fc258
Sibdepo server <info@sibdepo.ru>
2017-04-10 17:01:10 +0300

    all init in one file

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cp wp-config-local-sample.php wp-config-local.php
+[ ! -f ./wp-config-local.php ] && cp ./wp-config-local-sample.php ./wp-config-local.php
+
+cd ./sxd
+cp ./cfg-sample.php ./cfg.php
+cp ./ses-sample.php ./ses.php
+chmod 666 ./cfg.php ./ses.php
+chmod 777 ./backup
