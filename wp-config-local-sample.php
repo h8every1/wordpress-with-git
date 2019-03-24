48a7638fb7cc33c9232279b566a0042e58878d7b
Anton Syuvaev <syuvaev@ubic.tech>
2019-03-24 23:09:55 +0300

    use correct db host

--- wp-config-local-sample.php
+++ wp-config-local-sample.php
@@ -27,7 +27,7 @@ if ( EXTRA_DEBUG ) {
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db' );
 
 /**
  * Set default theme
