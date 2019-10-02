f749425b680dba5f47323e36b058f73195eecd13
Anton Syuvaev <syuvaev@ubic.tech>
2019-10-02 09:49:31 +0300

    add docker-compose override strategy`

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 [ ! -f ./wp-config-local.php ] && cp ./wp-config-local-sample.php ./wp-config-local.php
 
+[ ! -f ./docker-compose.override.yml ] && [ -f ./docker-compose.override.yml.dist ]  && cp ./docker-compose.override.yml.dist ./docker-compose.override.yml
+
 cd ./sxd
 cp ./cfg-sample.php ./cfg.php
 cp ./ses-sample.php ./ses.php
