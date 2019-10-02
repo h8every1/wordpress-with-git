2b90ef30c7aa99d097a38a1d7cd2d142aea10dd3
Anton Syuvaev <syuvaev@ubic.tech>
2019-10-02 10:19:32 +0300

    More detailed nginx config

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 [ ! -f ./wp-config-local.php ] && cp ./wp-config-local-sample.php ./wp-config-local.php
 
 [ ! -f ./docker-compose.override.yml ] && [ -f ./docker-compose.override.yml.dist ]  && cp ./docker-compose.override.yml.dist ./docker-compose.override.yml
+[ ! -f ./nginx.conf ] && touch ./nginx.conf
 
 cd ./sxd
 cp ./cfg-sample.php ./cfg.php
